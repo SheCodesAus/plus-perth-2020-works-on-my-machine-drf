521e10d09a43a745437d6f4e9a5dbd466a4957d4
MegsCodes <67173712+MOBrien1@users.noreply.github.com>
2020-10-24 10:55:47 +0800

    add user type to model

--- groupProjectBackend/users/models.py
+++ groupProjectBackend/users/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser): 
     name = models.CharField(max_length=200)
+    user_type = models.CharField(max_length=200)
     
     def __str__(self): 
         return self.username
--- groupProjectBackend/users/serializers.py
+++ groupProjectBackend/users/serializers.py
@@ -7,6 +7,8 @@ class CustomUserSerializer(serializers.Serializer):
     username = serializers.CharField(max_length=200)
     name = serializers..CharField(max_length=200)
     email = serializers.CharField(max_length=200)
+    user_type = serializers.CharField(max_length=200)
     
     def create(self, validated_data):
         return CustomUser.objects.create(**validated_data)
+
