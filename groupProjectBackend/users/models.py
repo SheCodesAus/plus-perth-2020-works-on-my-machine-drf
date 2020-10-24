3e129db9f436ca5a9dd3d97ec8e94d3f73114506
MegsCodes <67173712+MOBrien1@users.noreply.github.com>
2020-10-24 10:54:19 +0800

    add name to Custom User Model

--- groupProjectBackend/users/models.py
+++ groupProjectBackend/users/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser 
 
 class CustomUser(AbstractUser): 
-    pass 
+    name = models.CharField(max_length=200)
     
     def __str__(self): 
         return self.username
