179be5ec70e85b21c819b43b5d251cd2fc0db744
MegsCodes <67173712+MOBrien1@users.noreply.github.com>
2020-10-28 18:30:02 +0800

    uncommented URLs file

--- groupProjectBackend/mentors/urls.py
+++ groupProjectBackend/mentors/urls.py
@@ -1,9 +1,9 @@
-# from django.urls import path
-# from rest_framework.urlpatterns import format_suffix_patterns
-# from . import views
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from . import views
 
-# urlpatterns = [
-# path('mentors/', views.MentorList.as_view()),
-# ]
+urlpatterns = [
+path('mentors/', views.MentorList.as_view()),
+]
 
-# urlpatterns = format_suffix_patterns(urlpatterns)
+urlpatterns = format_suffix_patterns(urlpatterns)
