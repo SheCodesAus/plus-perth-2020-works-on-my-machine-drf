05c28b8b1d9575cab855e196cac854e585a77791
MegsCodes <67173712+MOBrien1@users.noreply.github.com>
2020-10-28 18:43:04 +0800

    uncomment url and views file

--- groupProjectBackend/mentors/views.py
+++ groupProjectBackend/mentors/views.py
@@ -1,12 +1,12 @@
 
-# from rest_framework.views import APIView
-# from rest_framework.response import Response
-# from .models import mentors
-# from .serializers import MentorProfileSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import mentors
+from .serializers import MentorProfileSerializer
 
-# class MentorProfileList(APIView):
+class MentorProfileList(APIView):
 
-# def get(self, request):
-#     mentors = Mentor.objects.all()
-#     serializer = MentorProfileSerializer(mentors, many=True)
-#     return Response(serializer.data)
+def get(self, request):
+    mentors = Mentor.objects.all()
+    serializer = MentorProfileSerializer(mentors, many=True)
+    return Response(serializer.data)
