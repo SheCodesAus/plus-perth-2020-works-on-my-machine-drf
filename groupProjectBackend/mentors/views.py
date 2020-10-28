ace283e2a5b1f5759cdbbdee85c5b6daaff62f6c
varunpreet-code <cp0452472@gmail.com>
2020-10-28 20:48:08 +0800

    middle of mentor profile list

--- groupProjectBackend/mentors/views.py
+++ groupProjectBackend/mentors/views.py
@@ -2,7 +2,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import MentorProfile, MentorProcess
-from .serializers import MentorProfileSerializer, MentorProcessSerializer 
+from .serializers import MentorProfileSerializer, MentorProcessSerializer
+
 
 class MentorProfileList(APIView):
 
@@ -10,4 +11,19 @@ class MentorProfileList(APIView):
         mentors = MentorProfile.objects.all()
         serializer = MentorProfileSerializer(mentors, many=True)
         return Response(serializer.data)
-       
+
+    def post(self, request):
+        serializer = MentorProfileSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+        return Response(serializer.data)
+
+
+class MentorProfileDetails(APIView):
+
+    def get(self, request, pk):
+        mentor = self.get_object(pk)
+        serializer = MentorDetailSerializer(mentor)
+        return Response(serializer.data)
+
+    
