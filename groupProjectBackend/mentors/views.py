d8ed53646237ee6fe413df8386ebff7112b7e69a
MegsCodes <67173712+MOBrien1@users.noreply.github.com>
2020-11-04 20:25:25 +0800

    Permissions Admin

--- groupProjectBackend/mentors/views.py
+++ groupProjectBackend/mentors/views.py
@@ -2,35 +2,31 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import MentorProfile, MentorProcess
 from .serializers import MentorProfileSerializer, MentorProcessSerializer
+from .permissions import BlacklistPermission
 from django.shortcuts import render
 from django.http import Http404
 from rest_framework import status, generics, permissions
-from rest_framework.permissions import (
-    BasePermission,
-    IsAuthenticatedOrReadOnly,
-    SAFE_METHODS,
-)
-
+from rest_framework.permissions import BasePermission, IsAuthenticated, IsAdminUser, SAFE_METHODS
 
 class MentorProcessDetail(generics.RetrieveUpdateDestroyAPIView):
-    #   permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAuthenticated, IsAdminUser, BlacklistPermission]
     queryset = MentorProcess.objects.all()
     serializer_class = MentorProcessSerializer
 
 
 class MentorProcessList(generics.ListAPIView):
-    #    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAuthenticated, IsAdminUser, BlacklistPermission]
     queryset = MentorProcess.objects.all()
     serializer_class = MentorProcessSerializer
 
 
 class MentorProfileDetail(generics.RetrieveUpdateDestroyAPIView):
-    #   permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAuthenticated, IsAdminUser, BlacklistPermission]
     queryset = MentorProfile.objects.all()
     serializer_class = MentorProfileSerializer
 
 
 class MentorProfileList(generics.ListCreateAPIView):
-    #    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAuthenticated, IsAdminUser, BlacklistPermission]
     queryset = MentorProfile.objects.all()
     serializer_class = MentorProfileSerializer
