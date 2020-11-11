e963ba40d7e5d1a0c5d3a00bdef9df97aff9dc11
MegsCodes <67173712+MOBrien1@users.noreply.github.com>
2020-11-11 18:21:51 +0800

    Updated URLS

--- groupProjectBackend/mentors/urls.py
+++ groupProjectBackend/mentors/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path("mentor_profile/<int:pk>/", views.MentorProfileDetail.as_view()),
     path("mentor_file_upload/", views.MentorFileUpload.as_view()),
     path("mentor_profile/<str:slug>/", views.MentorTypeList.as_view()),
-    path("mentor_skills/<str:slug>/", views.SkillsMentorList.as_view()),
+    path("mentor_skills/<str:slug>/", views.MentorSkillsList.as_view()),
     path("mentor_location/<str:slug>/", views.MentorLocationList.as_view()),
     path("mentor_type/<str:slug>/", views.MentorTypeList.as_view()),
 ]
